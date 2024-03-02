707c15a28563a5cc4a74f459c3abce3a61527487
danisherror <danishmsrit780@gmail.com>
2024-03-02 17:54:14 +0530

    added css to printfeedback and print room complaints

--- client/src/Student/PrintFeedBackData.js
+++ client/src/Student/PrintFeedBackData.js
@@ -3,9 +3,16 @@ import React, { useEffect, useState } from 'react'
 
 import { NavLink } from 'react-router-dom'
 
+import feedback from '../assets/img/feedback.jpg'
+import Feedback2 from '../assets/img/Feedback2.png'
+import Feedback1 from '../assets/img/Feedback1.png'
+import Feedback3 from '../assets/img/Feedback3.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 const PrintFeedBackData = () => {
 
-
+    const images = [Feedback1,Feedback2, Feedback3];
 
     const [feedback, setfeedback] = useState([]);
     const getToken = () => {
@@ -14,6 +21,34 @@ const PrintFeedBackData = () => {
     const token = getToken();
 
     console.log(token)
+    const containerStyle = {
+        display: 'flex',
+        height: '100vh',
+      };
+    
+      const halfStyle = {
+        width: '50%',
+        height: '100%',
+        overflow: 'hidden',
+        border: 'none', // Set border to none to make it invisible
+      };
+    
+      const leftHalfStyle = {
+        ...halfStyle,
+        backgroundColor: '#f2f2f2', // Optional: Add background color for the left half
+      };
+    
+      const rightHalfStyle = {
+        ...halfStyle,
+        width: '50%', // Adjust the width to your preference
+        backgroundImage: `url(${feedback})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      };
     const getdata = async () => {
 
         const res = await fetch(`/api/v1/getfeedback`, {
@@ -51,9 +86,8 @@ const PrintFeedBackData = () => {
 
     return (
         <>
-            <div>
-                {/* Display other components related to student profile */}
-                <div>
+            <div style={containerStyle}>
+                <div style={leftHalfStyle}>
                     <h2>Feedback Data</h2>
                     <table border="1">
                         <thead>
@@ -72,6 +106,33 @@ const PrintFeedBackData = () => {
                         </tbody>
                     </table>
                 </div>
+                <div style={rightHalfStyle}>
+        {/* Content for the right half goes here */}
+        <div >
+          <div className="w-3/4 z-0">
+            <Carousel
+              autoPlay={true}
+              infiniteLoop={true}
+              interval={3000}
+              showThumbs={false}
+              showStatus={false}
+              showArrows={false}
+              className="z-0"
+            >
+              {images.map((item, index) => (
+                <div key={index}>
+                  <img
+                    src={item}
+                    alt={`Room ${index + 1}`}
+                    className=""
+                    style={{ width: '75%', height: 'auto' }}
+                  />
+                </div>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </div>
             </div>
         </>
     )
--- client/src/Student/PrintRoomComplaint.js
+++ client/src/Student/PrintRoomComplaint.js
@@ -2,6 +2,12 @@
 import React, { useEffect, useState } from 'react'
 
 import { NavLink } from 'react-router-dom'
+import Issue from "../assets/img/Issue.jpg";
+import Room1 from "../assets/img/Room1.png";
+import Room3 from "../assets/img/Room3.png";
+import Room4 from "../assets/img/Room4.png";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const PrintRoomComplaints = () => {
 
@@ -14,6 +20,36 @@ const PrintRoomComplaints = () => {
     const token = getToken();
 
     console.log(token)
+    const images = [Room1, Room3, Room4];
+    const containerStyle = {
+        display: 'flex',
+        height: '100vh',
+    };
+
+    const halfStyle = {
+        width: '50%',
+        height: '100%',
+        overflow: 'hidden',
+        border: 'none', // Set border to none to make it invisible
+    };
+
+    const leftHalfStyle = {
+        ...halfStyle,
+        backgroundColor: '#f2f2f2', // Optional: Add background color for the left half
+
+    };
+
+    const rightHalfStyle = {
+        ...halfStyle,
+        width: '50%', // Adjust the width to your preference
+        backgroundImage: `url(${Issue})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    };
     const getdata = async () => {
 
         const res = await fetch(`/api/v1/getcomplain`, {
@@ -51,9 +87,9 @@ const PrintRoomComplaints = () => {
 
     return (
         <>
-            <div>
-                {/* Display other components related to student profile */}
-                <div>
+            <div style={containerStyle}>
+                
+                <div style={leftHalfStyle}>
                     <h2>Room Complaints Data</h2>
                     <table border="1">
                         <thead>
@@ -70,6 +106,33 @@ const PrintRoomComplaints = () => {
                         </tbody>
                     </table>
                 </div>
+                <div style={rightHalfStyle}>
+                {/* Content for the right half goes here */}
+                <div >
+                    <div className="w-3/4 z-0">
+                        <Carousel
+                            autoPlay={true}
+                            infiniteLoop={true}
+                            interval={3000}
+                            showThumbs={false}
+                            showStatus={false}
+                            showArrows={false}
+                            className="z-0"
+                        >
+                            {images.map((item, index) => (
+                                <div key={index}>
+                                    <img
+                                        src={item}
+                                        alt={`Room ${index + 1}`}
+                                        className=""
+                                        style={{ width: '75%', height: 'auto' }}
+                                    />
+                                </div>
+                            ))}
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
             </div>
         </>
     )
