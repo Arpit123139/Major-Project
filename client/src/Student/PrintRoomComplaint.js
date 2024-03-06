aef144b449349c55da25a7894015e981eab2828b
danisherror <danishmsrit780@gmail.com>
2024-03-06 22:57:22 +0530

    done changes on room complaint

--- client/src/Student/PrintRoomComplaint.js
+++ client/src/Student/PrintRoomComplaint.js
@@ -94,7 +94,7 @@ const PrintRoomComplaints = () => {
                 <div style={leftHalfStyle}>
                     <div class="card shadow-sm">
                         <div class="card-header bg-transparent border-0">
-                            <h3 class="mb-0"><i class="far fa-clone pr-1"></i>Feedback Data</h3>
+                            <h3 class="mb-0"><i class="far fa-clone pr-1"></i>Room Issue Data</h3>
                         </div>
                         <div class="card-body pt-0">
                             <table class="table table-bordered">
